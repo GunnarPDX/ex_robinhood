49758ed423c550f89deacaa8e2cefeca2e763799
Gunnar R <gunnarrosenberg@gmail.com>
2020-10-09 15:15:14 -0700

    login function up

--- lib/ex_robinhood.ex
+++ lib/ex_robinhood.ex
@@ -3,21 +3,54 @@ defmodule ExRobinhood do
   alias Enum, as: E
   alias ExRobinhood.Endpoints
 
+  use HTTPoison.Base
+
   @client_id "c82SH0WZOsabOXGP2sxqcj34FxkvfnWRZBKlBjFS"
 
-  @headers [
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5",
-    "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-    "X-Robinhood-API-Version": "1.0.0",
-    "Connection": "keep-alive",
-    "User-Agent": "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)"
-  ]
+  @headers %{
+    "Accept" => "*/*",
+    "Accept-Encoding" => "gzip, deflate",
+    "Accept-Language" => "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5",
+    "Content-Type" => "application/x-www-form-urlencoded; charset=utf-8",
+    "X-Robinhood-API-Version" => "1.0.0",
+    "Connection" => "keep-alive",
+    "User-Agent" => "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)"
+  }
 
-  def login(username, password) do
+  # ExRobinhood.login("example@example.com", "example_password", ExRobinhood.Util.gen_device_token)
 
+  def login(username, password, device_token) do
+    body = URI.encode_query(%{
+      "password" => password,
+      "username" => username,
+      "grant_type" => "password",
+      "client_id" => @client_id,
+      "expires_in" => "86400",
+      "scope" => "internal",
+      "device_token" => device_token,
+      "challenge_type" => "email"
+    })
+    |> IO.inspect()
+
+    resp = post(Endpoints.login, body, @headers)
+    #resp = nil
+
+    case resp do
+      {:ok, res} -> res.body |> Jason.decode!() |> IO.inspect()
+
+      {:error, message} ->
+        IO.inspect(Jason.decode!(message))
+        {:error, message}
+
+      _ -> {:error, "resp could not be processed"}
+
+    end
   end
+
+
+
+
+
 
   """
 
