ac39f2f15ee49429b8aa4609b2abc7739b8f2e78
Gunnar R <gunnarrosenberg@gmail.com>
2020-10-08 22:10:57 -0700

    first commit

---
+++ lib/ex_robinhood.ex
@@ -0,0 +1,18 @@
+defmodule ExRobinhood do
+  @moduledoc """
+  Documentation for `ExRobinhood`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExRobinhood.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_robinhood_test.exs
@@ -0,0 +1,8 @@
+defmodule ExRobinhoodTest do
+  use ExUnit.Case
+  doctest ExRobinhood
+
+  test "greets the world" do
+    assert ExRobinhood.hello() == :world
+  end
+end
